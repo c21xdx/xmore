712b0cff7aa0b711499362e0f2ec7794b07611ba
c21xdx <c21xdx@gmail.com>
2023-02-21 11:07:06 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 sed -i "s/8080/$PORT/" /app/config.json
-sed -i "s/ss_passwd/$PASSID/" /app/config.json
+sed -i "s/app_passid/$PASSID/" /app/config.json
 /app/app.js -c /app/config.json
